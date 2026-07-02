e19a98673f52c0dc535862c28ad8a36f9a1c7a53
Satyam Singh <168108005+satyam7611@users.noreply.github.com>
2026-07-02 15:26:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// let  inputTemp=document.getElementById("#input")
+// let fromU=document.getElementsByClassName(".fromU").value()
+// let toU=document.getElementsByClassName(".toU").value()
+
+// let btn=document.getElementsByClassName(".btn")
+
+
+// const handleSubmit=()=>{
+// let  inputTemp=Number(document.getElementById("#input")).value
+// let fromU=document.getElementsByClassName(".fromU").value
+// let toU=document.getElementsByClassName(".toU").value
+
+// let btn=document.getElementsByClassName(".btn")
+// let resultT=document.getElementsByClassName(".result")
+// let result=0
+
+// if(fromU==="fahrenheit" && toU==="celcius"){
+//     result=(inputTemp-32)(5/9)
+//     resultT.innerText=result;
+
+// }else if(fromU==="fahrenheit" && toU==="kelvin"){
+//     result=(inputTemp-32)(5/9)+273.15
+//     resultT.innerText=result;
+// } else if(fromU==="celcius" && toU==="fahrenheit"){
+//     result=inputTemp(9/5)+32;
+//     resultT.innerText=result
+    
+// }
+// else if(fromU==="celcius" && toU==="kelvin"){
+//     result=inputTemp+273.15
+//     resultT.innerText=result
+// }
+// else if(fromU==="kelvin" && toU==="celcius"){
+//     result=inputTemp-273.15;
+//     resultT.innerText=result
+// }
+// else if(fromU==="kelvin" && toU=="fahrenheit"){
+//     result=(inputTemp-273.15)(9/5)+32
+//     result.innerText=result
+// }
+
+
+// }
+
+// handleSubmit();
+
+
+
+// Celsius to Kelvin: K = C + 273.15
+// Kelvin to Celsius: C = K - 273.15
+// Fahrenheit to Celsius: C = (F-32) (5/9)..
+// Celsius to Fahrenheit: F = C(9/5) + 32
+// Fahrenheit to Kelvin: K = (F-32) (5/9) + 273.15..
+// Kelvin to Fahrenheit: F = (K-273.15) (9/5) + 32
